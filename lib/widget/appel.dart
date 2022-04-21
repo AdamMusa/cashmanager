ad4ef55ea3716dfe0f4e83ee8ee21508ca2297f4
AdamMusa <adammusa2222@gmail.com>
2022-04-21 10:52:55 +0100

    added all items and create super transition

--- lib/widget/appel.dart
+++ lib/widget/appel.dart
@@ -1,12 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class Appels extends StatelessWidget {
   const Appels({ Key? key }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Hello from appel"),
+    return Scaffold(
+      appBar: AppBar(),
+      body: ListView.builder(
+        itemCount: 12,
+        itemBuilder: (context,int index)=>
+        Padding(
+          padding: const EdgeInsets.only(left:8,right: 8,top:8),
+          child: Card(
+            elevation: 8,
+            child: ListTile(
+              onTap: ()=>Get.de,
+              title: Text("option $index"),
+              trailing: const Icon(Icons.check),
+            ),
+          ),
+        )
+      ),
     );
   }
 }
--- lib/widget/index.dart
+++ lib/widget/index.dart
@@ -1,63 +1,70 @@
+import 'package:cashmanager/widget/appel.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:lottie/lottie.dart';
 
 class Index extends StatelessWidget {
   const Index({Key? key}) : super(key: key);
 
+
+Widget customItem({VoidCallback? onTap,String? lottiePath,String? text }){
+  return GestureDetector(
+    onTap: onTap,
+  child:  Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Lottie.asset(lottiePath!,height: 150),
+          const SizedBox(height: 10,),
+          Text(text!,style: const TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 16
+          ),)
+        ],
+      ),
+  );
+}
+
   @override
   Widget build(BuildContext context) {
     return GridView.count(
       primary: false,
-      padding: const EdgeInsets.all(20),
+      padding: const EdgeInsets.only(left: 20,right: 20,bottom: 20),
       crossAxisSpacing: 10,
       mainAxisSpacing: 10,
       crossAxisCount: 2,
       children: <Widget>[
-        
-        Column(
-          mainAxisSize: MainAxisSize.min,
-
-          children: [
-            Lottie.asset("assets/moov_internet.json",height: 150),
-            const SizedBox(height: 10,),
-            const Text("Activez les Appels")
-          ],
+        customItem(
+          onTap: ()=>Get.to(
+            const Appels(),
+            transition: Transition.zoom
+            ),
+          lottiePath: "assets/lottie/moov_internet.json",
+          text: "Activez les Appels"
         ),
-       
-        Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Lottie.asset("assets/moov_msg.json",height: 150),
-            const SizedBox(height: 10,),
-            const Text("Activez les SMS")
-          ],
+        customItem(
+          onTap: (){},
+          lottiePath: "assets/lottie/moov_msg.json",
+          text: "Activez les SMS"
         ),
-        Column(
-          mainAxisSize: MainAxisSize.min,
-
-          children: [
-            Lottie.asset("assets/moov.json",height: 150),
-            const SizedBox(height: 10,),
-            const Text("Rechargez les Megas")
-
-          ],
+        customItem(
+          onTap: (){},
+          lottiePath: "assets/lottie/moov.json",
+          text: "Rechargez les Megas"
         ),
-        Column(
-          mainAxisSize: MainAxisSize.min,
-
-          children: [
-            Lottie.asset("assets/moov_money.json",height: 150),
-            const SizedBox(height: 10,),
-            const Text("Envoyez l'argent")
-          ],
+        customItem(
+          onTap: (){},
+          lottiePath: "assets/lottie/moov_money.json",
+          text: "Envoyez l'argent"
         ),
-        Column(
-          children: [
-            Lottie.asset("assets/moov_success.json",height: 150),
-            const SizedBox(height: 10,),
-            const Text("Activez les Appels")
-
-          ],
+        customItem(
+          onTap: (){},
+          lottiePath: "assets/lottie/reabonner.json",
+          text: "Reabonnez"
+        ),
+        customItem(
+          onTap: (){},
+          lottiePath: "assets/lottie/electricity.json",
+          text: "Rechargez l'electricité"
         ),
       ],
     );
@@ -74,3 +81,4 @@ class Index extends StatelessWidget {
 // https://edit.lottiefiles.com/?src=https%3A%2F%2Fassets2.lottiefiles.com%2Fpackages%2Flf20_rLfMZE.json
 // https://edit.lottiefiles.com/?src=https%3A%2F%2Fassets4.lottiefiles.com%2Fpackages%2Flf20_pjomybim.json
 // https://edit.lottiefiles.com/?src=https%3A%2F%2Fassets4.lottiefiles.com%2Fpackages%2Flf20_zvmuqszh.json
+// https://edit.lottiefiles.com/?src=https%3A%2F%2Fassets10.lottiefiles.com%2Fprivate_files%2Flf30_fbn8elpr.json
