62002db2ec68b46be173cb732f1fe19dd45b29c1
AdamMusa <adammusa2222@gmail.com>
2022-05-05 20:34:37 +0100

    refactor code

--- lib/database/database.dart
+++ lib/database/database.dart
@@ -3,15 +3,15 @@ import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 
 class DataBase extends GetxController{
-  var katir_appel = [].obs;
-  var katir_appel_mix = [].obs;
+  var katirAppel = [].obs;
+  var katirAppelMix = [].obs;
   readDataFromDatabase()async{
    String data =  await rootBundle.loadString("assets/databasemoov.json");
    final jsonResul = jsonDecode(data);
   //  print(jsonResul);
-    katir_appel.value = jsonResul["Moov"]["Kattir Appels Moov"];
-    katir_appel_mix.value = jsonResul["Moov"]["Katir MIX"];
-    print(katir_appel_mix);
+    katirAppel.value = jsonResul["Moov"]["Kattir Appels Moov"];
+    katirAppelMix.value = jsonResul["Moov"]["Katir MIX"];
+    print(katirAppelMix);
     // katir_appel_mix.value = jsonResul["Moov"]["Katir MIX"];
     // print(appels);
   }
