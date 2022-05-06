dc39a61fd385f9b36225ac0f429e5c7fa85900b0
AdamMusa <adammusa2222@gmail.com>
2022-05-06 14:55:41 +0100

    refactor

--- lib/widget/send_money.dart
+++ lib/widget/send_money.dart
@@ -1,97 +1,88 @@
 import 'package:delayed_display/delayed_display.dart';
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
+
 class SendMoney extends StatelessWidget {
-  SendMoney({ Key? key }) : super(key: key);
+  SendMoney({Key? key}) : super(key: key);
   final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.indigo,
-        title:const DelayedDisplay(
-          delay: Duration(seconds: 1),
-          child: Text("Envoyez l'argent"))
-      ),
+          backgroundColor: Colors.indigo,
+          title: const DelayedDisplay(
+              delay: Duration(seconds: 1), child: Text("Envoyez l'argent"))),
       body: Container(
         alignment: Alignment.center,
-        padding: const EdgeInsets.only(left:15,right: 15),
+        padding: const EdgeInsets.only(left: 15, right: 15),
         child: SingleChildScrollView(
           child: Form(
-            key: _formKey,
-            autovalidateMode: AutovalidateMode.always,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                  Lottie.asset("assets/lottie/moov_money.json",height: 200),
-                  const SizedBox(height: 20,),
+              key: _formKey,
+              autovalidateMode: AutovalidateMode.always,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Lottie.asset("assets/lottie/moov_money.json", height: 200),
+                  const SizedBox(
+                    height: 20,
+                  ),
                   TextFormField(
                     keyboardType: TextInputType.number,
                     decoration: InputDecoration(
-                      hintText: "Entrer le numero du beneficiaire",
-                      suffixIcon: const Icon(Icons.perm_identity),
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: const  BorderSide(
-                          color:Colors.indigo
-                        ),
-                        borderRadius: BorderRadius.circular(20)
-                      ),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(20)
-                      )
-                    ),
+                        hintText: "Entrer le numero du beneficiaire",
+                        suffixIcon: const Icon(Icons.perm_identity),
+                        enabledBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(color: Colors.indigo),
+                            borderRadius: BorderRadius.circular(20)),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20))),
                   ),
-                  const SizedBox(height: 15,),
-                  TextFormField(
-                      keyboardType: TextInputType.number,
-                      decoration: InputDecoration(
-                      hintText: "Entrer le montant",
-                     suffixIcon: const Icon(Icons.monetization_on),
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: const  BorderSide(
-                          color:Colors.indigo
-                        ),
-                        borderRadius: BorderRadius.circular(20)
-                      ),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(20)
-                      )
-                    ),
+                  const SizedBox(
+                    height: 15,
                   ),
-                  const SizedBox(height: 15,),
                   TextFormField(
                     keyboardType: TextInputType.number,
                     decoration: InputDecoration(
-                      hintText: "Entrer votre mot de passe",
-                     suffixIcon: const Icon(Icons.visibility),
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: const  BorderSide(
-                          color:Colors.indigo
-                        ),
-                        borderRadius: BorderRadius.circular(20)
-                      ),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(20)
-                      )
-                    ),
+                        hintText: "Entrer le montant",
+                        suffixIcon: const Icon(Icons.monetization_on),
+                        enabledBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(color: Colors.indigo),
+                            borderRadius: BorderRadius.circular(20)),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20))),
                   ),
-                  const SizedBox(height: 20,),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  TextFormField(
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                        hintText: "Entrer votre mot de passe",
+                        suffixIcon: const Icon(Icons.visibility),
+                        enabledBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(color: Colors.indigo),
+                            borderRadius: BorderRadius.circular(20)),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20))),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
                   DelayedDisplay(
                     delay: const Duration(seconds: 1),
-                    child: ElevatedButton(onPressed: (){}, 
-                    child: const Text("Envoyez"),
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.indigo,
-                      minimumSize: const Size(200,40),
-                      shape: const StadiumBorder(),
-                      elevation: 4
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: const Text("Envoyez"),
+                      style: ElevatedButton.styleFrom(
+                          primary: Colors.indigo,
+                          minimumSize: const Size(200, 40),
+                          shape: const StadiumBorder(),
+                          elevation: 4),
                     ),
-                                  ),
                   )
-             ],
-            )
-          ),
+                ],
+              )),
         ),
       ),
     );
