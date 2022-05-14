c388c49ee19094941bc3257e2eb13f9ed5a8bebd
AdamMusa <adammusa2222@gmail.com>
2022-05-14 15:56:58 +0100

    added localdb

--- lib/database/local_database.dart
+++ lib/database/local_database.dart
@@ -1,20 +1,20 @@
-// import 'package:objectbox/objectbox.dart';
+import 'package:objectbox/objectbox.dart';
 
-// @Entity()
-// class LocalDatabase {
+@Entity()
+class LocalDatabase {
 
-//   late int id;
-//   late String price;
-//   late String min;
-//   late String sms;
-//   late String data;
-//   late String validity;
-//   late String code;
-//   late String titre;
-//   late String description;
-//   bool? pending = false;
+  late int id;
+  late String price;
+  late String min;
+  late String sms;
+  late String data;
+  late String validity;
+  late String code;
+  late String titre;
+  late String description;
+  bool? pending = false;
 
-//   LocalDatabase({this.id = 0, required this.price, required this.min,required this.sms,required this.data , required this.validity,required this.code,required this.titre,required this.description,this.pending});
+  LocalDatabase({this.id = 0, required this.price, required this.min,required this.sms,required this.data , required this.validity,required this.code,required this.titre,required this.description,this.pending});
 
   
-// }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,22 @@
+import 'package:cashmanager/controller/home_controller.dart';
 import 'package:cashmanager/widget/home.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:get/get.dart';
 
-void main() {
+void main() async{
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   // await Future.delayed(const Duration(seconds: 3));
   FlutterNativeSplash.remove();
- 
+  final controller = Get.put(HomeController());
+  final store = await controller.awaitStore();
   runApp(
     GetMaterialApp(
     debugShowCheckedModeBanner: false,
     home: Home(),
   ));
+
+  store.close();
 
 }
