dbb1ade3c1cb28b3960173198ffb572d8af77c85
AdamMusa <adammusa2222@gmail.com>
2022-05-20 11:27:14 +0100

    initial test

--- lib/controller/home_controller.dart
+++ lib/controller/home_controller.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:lottie/lottie.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:ussd_advanced/ussd_advanced.dart';
 import '../database/database.dart';
 import '../database/local_database.dart';
 import 'dart:collection';
@@ -25,6 +26,7 @@ class HomeController extends GetxController{
    final db = Get.put(DataBase());
    late final Box<LocalDatabase> box;
    late Store _store;
+   var msg = "".obs;
 
    switcher(value){
      index.value = value;
@@ -53,7 +55,6 @@ class HomeController extends GetxController{
     _store.close();
     super.onClose();
   }
-  
 
   openDialog(){
     return Get.defaultDialog(
@@ -61,9 +62,9 @@ class HomeController extends GetxController{
       content: Column(
         children: [
           Lottie.asset("assets/lottie/moov_success.json",height: 140),
-          const Padding(
-            padding: EdgeInsets.only(left: 10,right:10),
-            child: Text("Votre demande est en cours de traitement",textAlign: TextAlign.center, style: TextStyle(fontWeight: FontWeight.bold),),
+          Padding(
+            padding: const EdgeInsets.only(left: 10,right:10),
+            child: Text(msg.value.toString(),textAlign: TextAlign.center, style: TextStyle(fontWeight: FontWeight.bold),),
           )
           ]
       )
@@ -79,18 +80,30 @@ class HomeController extends GetxController{
   }
 
   formaterHelper(code,value){
-    var newValue = code.replaceAll(RegExp('(moi ou autre)'), '');
+    var newValue = code.replaceAll(RegExp('(1 ou 2)'), '');
     return newValue.replaceAll(RegExp(r'\(.*\)'), value);
   }
-  sendCode(code,value){
-    print(formater(code, value));
-    // UssdAdvanced.sendAdvancedUssd(subscriptionId: 1,code: formater(code, value));
+  sendCode(code,value)async{
+    // print(formater(code, value));
+    final  res = await UssdAdvanced.sendAdvancedUssd(subscriptionId: 1,code: formater(code, value));
+    if(res !=null && res != "Votre balance est insuffisante pour le service"){
+      msg.value = res;
+    }
+    else{
+       msg.value = "Votre balance est insuffisante pour le service";
+    }
   }
 
-  sendDirectCode(code){
-    print(code);
+  sendDirectCode(code)async{
+    // print(code);
 
-    // UssdAdvanced.sendAdvancedUssd(subscriptionId: 2,code: code);
+    final res = await UssdAdvanced.sendAdvancedUssd(subscriptionId: 2,code: code);
+     if(res !=null && res != "Votre balance est insuffisante pour le service"){
+      msg.value = res;
+    }
+    else{
+       msg.value = "Votre balance est insuffisante pour le service";
+    }
   }
 
 }
