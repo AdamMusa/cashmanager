76a8f37f9660355f23ce203a17ce9ff0e0433fc1
AdamMusa <adammusa2222@gmail.com>
2022-05-21 08:43:14 +0100

    added data automatically

--- lib/widget/transaction.dart
+++ lib/widget/transaction.dart
@@ -9,31 +9,25 @@ class Transaction extends GetView<TransactionController>{
   final transction = Get.put(TransactionController());
   @override
   Widget build(BuildContext context) {
+    print(transction.listRecordePending);
     // print(controller.box.getAll().where((element) => element.pending==false));
-    return GetBuilder(
-        
-        builder: (controller){
-          if(controller==null){
-            return const Text("empty");
-          }
-          return ListView.builder(
-            itemCount: transction.listRecordePending.length,
-            itemBuilder: (context, index) => Container(
-            height: 100,
-            width: MediaQuery.of(context).size.width,
-            padding: const EdgeInsets.only(left: 10,right: 10,top: 10),
-          
-            child: Card(
-              elevation: 4,
-              child: Column(
-                children: [
-                  Text(transction.listRecordePending[index].validity.toString())
-                ],
-              ),
-            )
-          )
-        );
-      }
+    return transction.listRecordePending.isEmpty?const Text("no data yet"): ListView.builder(
+        itemCount: transction.listRecordePending.length,
+        itemBuilder: (context, index) => Container(
+        height: 100,
+        width: MediaQuery.of(context).size.width,
+        padding: const EdgeInsets.only(left: 10,right: 10,top: 10),
+      
+        child: Card(
+          elevation: 4,
+          child: Column(
+            children: [
+              Text(transction.listRecordePending[index].validity.toString())
+            ],
+          ),
+        )
+      )
     );
+   
   }
 }
